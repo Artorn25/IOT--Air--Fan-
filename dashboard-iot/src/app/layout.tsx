46c361fed37af20ab26932a7d4a40e7fa25a6452
Artorn25 <sskjj0547@gmail.com>
2025-06-15 23:27:56 +0700

    style: change font to "Ubuntu"

--- dashboard-iot/src/app/layout.tsx
+++ dashboard-iot/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Ubuntu } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata: Metadata = {
   title: 'IoT Fan Healthy',
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu.className}>{children}</body>
     </html>
   );
 }
